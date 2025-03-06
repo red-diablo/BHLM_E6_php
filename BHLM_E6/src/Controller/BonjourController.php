5f27c4241d934bc050543174f93e8fa05b00ead0
Solène Langlois <solene.langlois@campus-la-chataigneraie.org>
2025-03-06 12:58:58 +0100

    Page de login (que du texte pour l'instant)

--- BHLM_E6/src/Controller/BonjourController.php
+++ BHLM_E6/src/Controller/BonjourController.php
@@ -1,10 +1,12 @@
 <?php
 namespace App\Controller;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 
-    class BonjourController {
+    class BonjourController extends AbstractController{
         function bonjour(){
-            return new Response('Bonjour');
+            $titre="Bonjour, connectez-vous :";
+            return $this->render('base.html.twig', ['titreDeLaPage' => $titre]);
         }
     }
 
