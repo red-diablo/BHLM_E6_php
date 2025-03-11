656acdfe679a30777caea182f1116dad911468d4
Solène Langlois <solene.langlois@campus-la-chataigneraie.org>
2025-03-11 12:38:54 +0100

    page de connexion dynamique

--- BHLM_E6/src/Formulaire/ConnexionType.php
+++ BHLM_E6/src/Formulaire/ConnexionType.php
@@ -1,4 +1,4 @@
-<!-- FORMULAIRE DE CONNEXION QUAND ON AURA CREER LES ENTITES
+<?php
 namespace App\Formulaire;
 
 use Symfony\Component\Form\AbstractType;
@@ -6,13 +6,15 @@ use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use App\Entity\Utilisateur;
 
 class ConnexionType extends AbstractType {
 
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $builder->add('login', TextType :: class, ['label' => 'Login'])
-        ->add('password', PasswordType :: class, ['label' => 'Mot de passe'])
-        ->add ('submit', SubmitType :: class, ['label' => 'Se connecter'])
+        $builder->add('Login', TextType :: class, ['label' => 'Login'])
+        ->add('Mdp', PasswordType :: class, ['label' => 'Mot de passe'])
+        ->add ('submit', SubmitType :: class, ['label' => 'Se connecter']);
     }
-} -->
+}
+?>
