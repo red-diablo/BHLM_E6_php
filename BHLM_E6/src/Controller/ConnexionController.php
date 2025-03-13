3d1cf953001183141baec43ec42a16e4a4406521
kylan mercher <kylan.mercher@camus-la-chataigeraie.org>
2025-03-13 09:52:00 +0100

    Revert "Merge branch 'main' into kylan"
    This reverts commit 0e5a1ea3f65bfc0d93b65a6e2433ac10643f7166, reversing
    changes made to bd6303c19d694dc2b4eed674083d30ff7dded24c.

--- BHLM_E6/src/Controller/ConnexionController.php
+++ BHLM_E6/src/Controller/ConnexionController.php
@@ -1,41 +1,29 @@
 <?php
 namespace App\Controller;
 
-use App\Entity\Utilisateur;
-use App\Formulaire\ConnexionType;
-use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
-    class ConnexionController extends AbstractController
-    {
-        #[Route ('/connexion')]
-        public function appelConnexionForm(Request $request, EntityManagerInterface $entityManager)
-        {
-             $utilisateur = new Utilisateur();
-             $connexionForm = $this->createForm(ConnexionType :: class, $utilisateur);
-             $connexionForm->handleRequest($request);
+    class ConnexionController extends AbstractController{
+        public function connexion(){
+            $titre="Bonjour, connectez-vous :";
+            return $this->render('base.html.twig', ['titreDeLaPage' => $titre]);
+        }
 
-            if ($connexionForm->isSubmitted() && $connexionForm->isValid()) 
-            {
-                // Chercher l'utilisateur en base de données
-                $repo = $entityManager->getRepository(Utilisateur::class);
-                $user = $repo->findOneBy(['login' => $utilisateur->getLogin()]);
-                if ($user && password_verify($utilisateur->getMdp(), $user->getMdp())) {
-                    // Connexion réussie, rediriger vers la page d'accueil (liste des entreprises)
-                    return $this->redirectToRoute('accueil');
-                }
-                else {
-                    return $this->render('connexion.html.twig', ['connexionForm'=> $connexionForm->createView()]);
-                }
-            }
-            else
-            {
-                return $this->render('connexion.html.twig', ['connexionForm'=> $connexionForm->createView()]);
-            }
-         }
+        // public function appelConnexionForm(){
+        //     $form = new Connexion;
+        //     $connexionForm = $this->createForm(ConnexionType :: class, $form);
+        // if ($form->isSubmitted() && $form->isValid()) {
+        //     $data = $form->getData();
+        //     $login = $entityManager->getRepository(NomTable::class)->findOneBy(['login' => $data['login']]);
+
+        //     if (!$login || !$passwordEncoder->isPasswordValid($login, $data['password'])) {
+        //         $this->addFlash('error', 'Identifiants incorrects');
+        //     } else {
+        //         // Gérer la connexion (via un système de session ou Symfony Security) VOIRE DOCUMENTATION SYMFONYY
+        //     }
+        // }
+        //     return $this->render('base.html.twig', ['connexionForm'=> $connexionForm->createView()]);
+        // }
     }
 ?>
--- BHLM_E6/vendor/composer/autoload_files.php
+++ BHLM_E6/vendor/composer/autoload_files.php
@@ -17,6 +17,4 @@ return array(
     'ffecb95d45175fd40f75be8a23b34f90' => $vendorDir . '/twig/twig/src/Resources/debug.php',
     'c7baa00073ee9c61edf148c51917cfb4' => $vendorDir . '/twig/twig/src/Resources/escaper.php',
     'f844ccf1d25df8663951193c3fc307c8' => $vendorDir . '/twig/twig/src/Resources/string_loader.php',
-    '2203a247e6fda86070a5e4e07aed533a' => $vendorDir . '/symfony/clock/Resources/now.php',
-    '6a47392539ca2329373e0d33e1dba053' => $vendorDir . '/symfony/polyfill-intl-icu/bootstrap.php',
 );
