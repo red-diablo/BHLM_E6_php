b3b763ca4f9217b5a68351a6c044d87af0b0bf76
Solène Langlois <solene.langlois@campus-la-chataigneraie.org>
2025-04-03 12:48:17 +0200

    Page inscription

---
+++ BHLM_E6/migrations/Version20250403072934.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20250403072934 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE specialite (id INT AUTO_INCREMENT NOT NULL, nom VARCHAR(50) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE etudiant ADD specialite_id INT DEFAULT NULL');
+        $this->addSql('ALTER TABLE etudiant ADD CONSTRAINT FK_717E22E32195E0F0 FOREIGN KEY (specialite_id) REFERENCES specialite (id)');
+        $this->addSql('CREATE INDEX IDX_717E22E32195E0F0 ON etudiant (specialite_id)');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE etudiant DROP FOREIGN KEY FK_717E22E32195E0F0');
+        $this->addSql('DROP TABLE specialite');
+        $this->addSql('DROP INDEX IDX_717E22E32195E0F0 ON etudiant');
+        $this->addSql('ALTER TABLE etudiant DROP specialite_id');
+    }
+}
