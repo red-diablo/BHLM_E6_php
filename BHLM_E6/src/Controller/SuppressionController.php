55d3ca2bd35aa0eeb89de2df2ef60d49e0963d5e
kylan mercher <kylan.mercher@camus-la-chataigeraie.org>
2025-04-28 10:37:20 +0200

    suppression

--- BHLM_E6/src/Controller/SuppressionController.php
+++ BHLM_E6/src/Controller/SuppressionController.php
@@ -7,13 +7,38 @@ use App\Form\SupprType;
 use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Response;
 
 class SuppressionController extends AbstractController
 {
-    public function appelFormSuppression(ManagerRegistry $doctrine, Request $request): Response
+    public function appelFormSuppression(Entreprise $entreprise): Response
     {
-        return $this->render('suppression.twig');
+        return $this->render('suppression.twig', [
+            'entreprise' => $entreprise,
+        ]);
+    }
+
+
+
+    public function suppression(Request $request, Entreprise $entreprise, EntityManagerInterface $entityManager): Response
+    {
+        if ($this->isCsrfTokenValid('suppr_confirm' . $entreprise->getId(), $request->request->get('_token'))) {
+            // Supprimer tous les employés liés à cette entreprise
+            foreach ($entreprise->getEmployes() as $employe) {
+                $entityManager->remove($employe);
+            }
+    
+            // Supprimer l'entreprise
+            $entityManager->remove($entreprise);
+            $entityManager->flush();
+    
+           
+        } else {
+            $this->addFlash('impossible de supprimer l entreprise .');
+        }
+
+        return $this->redirectToRoute('accueil');
     }
 }
 ?>
